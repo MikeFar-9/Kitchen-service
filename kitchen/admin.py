42f2ffc8e6e57c5fbc0096aaa3ff5d6cd959cf07
Mike Far <mikefarenyuk@gmail.com>
2025-04-30 11:44:49 +0300

    add file admin.py

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Cook, DishType, Ingredient, Dish, DishIngredient
 
-# Register your models here.
+
+admin.site.register(Cook, UserAdmin)
+admin.site.register(DishType)
+admin.site.register(Ingredient)
+admin.site.register(Dish)
+admin.site.register(DishIngredient)
