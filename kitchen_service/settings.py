7a068680bb8109bdb88e14238a1109f7bbc7ca92
Mike Far <mikefarenyuk@gmail.com>
2025-05-04 11:13:09 +0300

    create file .env and add it to .gitingore, update file settings.py, update file requirements.txt

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%!ar=sy+#ps=do9jl2*1win8eaxor^c2p#@7pkm^%(6^rhxuxf"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
